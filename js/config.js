c48794cd0f48606a41c0d9851142ea773e1410ee
CroutonsHD <30559024+CroutonsHD@users.noreply.github.com>
2018-01-06 01:40:39 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@
 /*URL for the server logo
 Leave it blank to use text instead
 (Put the files in the images folder)*/
-var l_serverImage = "ZealotGaming.png";
+var l_serverImage = "logo_current.png";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
